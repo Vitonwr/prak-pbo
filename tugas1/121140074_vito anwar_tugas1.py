7f26575f2bd5e3e2c702cdcba0e46cef1bb1cc8f
Vitonwr <126578164+Vitonwr@users.noreply.github.com>
2023-03-04 08:50:22 +0700

    Update 121140074_vito anwar_tugas1.py

--- tugas1/121140074_vito anwar_tugas1.py
+++ tugas1/121140074_vito anwar_tugas1.py
@@ -1,1 +1,19 @@
+#Vito Anwar_121140074_rb
 
+username = ("informatika")
+password = ("12345678")
+
+i = int(1)
+
+while(i <= 3):
+   user = input("masukan username : ")
+   passw = input("masukan password : ")
+   if(user == username and passw == password):
+      print("Login Berhasil")
+      break
+   elif(i == 3):
+      print("Akun anda diblokir")
+      break
+   else:
+      print("Username atau Password salah coba lagi")
+      i += 1
